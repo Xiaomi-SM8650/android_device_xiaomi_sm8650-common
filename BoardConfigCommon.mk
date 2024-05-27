6fe76c4b30a6526b20f76ec64a282938d101efd5
kmiit <2421768138@qq.com>
2024-05-28 03:02:23 +0800

    sm8650-common: Initial empty common device tree

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/xiaomi/sm8650-common/BoardConfigVendor.mk
+
+COMMON_PATH := device/xiaomi/sm8650-common
