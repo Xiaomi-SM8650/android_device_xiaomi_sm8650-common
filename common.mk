52fd794596f82ec1ef16dbc921c592304c3e4ad3
kmiit <2421768138@qq.com>
2024-05-29 01:49:46 +0800

    sm8650-common: Add API level

--- common.mk
+++ common.mk
@@ -7,6 +7,11 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sm8650-common/sm8650-common-vendor.mk)
 
+# API level
+BOARD_API_LEVEL := 34
+BOARD_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
