c9a059095d4621f38578c61ea40422f24d4e35ac
kmiit <2421768138@qq.com>
2024-05-29 01:39:40 +0800

    sm8650-common: soong: Define local soong namespace

--- common.mk
+++ common.mk
@@ -6,3 +6,7 @@
 
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sm8650-common/sm8650-common-vendor.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
