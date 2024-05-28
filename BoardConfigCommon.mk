b0264b9110d1df18587e0f9d6a0cf718c1dac9a3
kmiit <2421768138@qq.com>
2024-05-28 12:47:18 +0800

    sm8650-common: gpt-utils: Import gpt-utils from CLO * From https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/recovery-ext * Tag: LA.VENDOR.14.3.0.r1-12600-lanai.QSSI15.0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,6 +16,9 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo300
 
+# Boot control
+$(call soong_config_set, ufsbsg, ufsframework, bsg)
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := pineapple
 TARGET_NO_BOOTLOADER := true
